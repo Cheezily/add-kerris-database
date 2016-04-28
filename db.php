92fad21782a6ed32b38bfdebd8d94f778f3a9808
cheezily <pm655@yahoo.com>
2016-04-28 14:27:02 -0500

    add ability to remove entries once logged in and start ability to add new courses

--- db.php
+++ db.php
@@ -8,7 +8,7 @@
 DB has 4 columns: id (INT), short_name (TEXT), long_name (TEXT), semester (TEXT)
 *******************************************************************************/
 
-if ($_GET['q'] == "showAll") {
+if (isset($_GET['q']) && $_GET['q'] == "showAll") {
   db_query('SELECT * FROM courses');
 }
 
@@ -17,13 +17,33 @@ $user = parse_ini_file('config/user.ini');
 if (isset($_POST['userName']) && isset($_POST['PW']) &&
   $_POST['userName'] == $user['name'] &&
   $_POST['PW'] == $user['password']) {
-    echo "CORRECT!!!";
-    $conn = db_connect();
+    //$conn = db_connect();
+    action_router($_POST);
   } else if (isset($_POST['userName']) && isset($_POST['PW']) &&
     $_POST['userName'] != $user['name'] &&
     $_POST['PW'] != $user['password']) {
     echo "Invalid";
   }
+
+
+function action_router($Action) {
+  //echo "Action sent was ".$Action['Action'];
+
+  if ($Action['Action'] == 'Check') {
+    echo 'Correct';
+  }
+
+  if ($Action['Action'] == 'Remove') {
+    $sql = 'DELETE FROM courses WHERE id = '.$Action['id'].';';
+    //echo "TRYING TO REMOVE";
+    $connection = db_connect();
+    $result = mysqli_query($connection, $sql);
+    //echo "RESULT: ".$result;
+    db_query('SELECT * FROM courses');
+  }
+}
+
+
 
 function db_connect() {
   $config = parse_ini_file('config/config.ini');
@@ -62,16 +82,21 @@ function db_select($query) {
   $connection = db_connect();
   $result = mysqli_query($connection, $query);
 
-  //insert commands will return true
-  if ($result === false) {
-    return db_error();
+  if (count($result) == 0) {
+    echo "NO RECORDS";
   } else {
-    $rows = array();
-    while($row = mysqli_fetch_assoc($result)) {
-      $rows[] = $row;
+    //insert commands will return true
+    if ($result === false) {
+      return db_error();
+    } else {
+      $rows = array();
+      while($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+      }
+      return $rows;
     }
-    return $rows;
   }
+
 }
 
 ?>
--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
+var username = '';
+var password = '';
+
+
 $(document).ready(function() {
   $.get({
     url: 'db.php',
     data: "q=showAll",
     success: function(response) {
-      $('.currentInfo').html(showCurrent(JSON.parse(response)));
+      if (response.length == 0) {
+        $('.output').html("No results to show...");
+      } else {
+        $('.output').html(showCurrent(JSON.parse(response)));
+      }
     }
   })
-
 });
 
 
 function showCurrent(response) {
   var html = "<table><th>Class Code</th>\
-  <th>Class Name</th><th>Semester</th>";
+  <th>Class Name</th><th>Semester</th><th>Action</th>";
   for (row in response) {
-    html += "<tr><td>" + response[row]['short_name'] + "</td><td>" + response[row]['long_name'] + "</td><td>" + response[row]['semester'] + "</td></tr>";
+    html += "<tr><td>" + response[row]['short_name'] +
+    "</td><td>" + response[row]['long_name'] +
+    "</td><td>" + response[row]['semester'] + "</td>" +
+    "<td><span class='remove' onClick=removeItem(" + response[row]['id'] + ")>Remove</span></td> "
+    "</tr>";
   }
-
   html += "</table>"
   return html;
 }
@@ -24,27 +34,66 @@ function showCurrent(response) {
 
 $('#sendLogin').click(function(event) {
   event.preventDefault();
-
   $('#warning').text('');
-
-  var uname = $('#username').val();
-  var pw = $('#password').val();
-
-  var data = "userName=" + uname + "&PW=" + pw;
-
-  $.post({
-    url: 'db.php',
-    data: data,
-    success: function(response) {
-      if (response == "Invalid") {
-        $('#warning').text("Invalid username or password");
-      } else {
-        console.log("response: " + response);
-        $('.output').append(response);
+  username = $('#username').val();
+  password = $('#password').val();
+  if (username && password) {
+    var data = "userName=" + username + "&PW=" + password + "&Action=Check";
+    $.post({
+      url: 'db.php',
+      data: data,
+      success: function(response) {
+        if (response == "Invalid") {
+          $('#warning').text("Invalid username or password");
+          username = '';
+          password = '';
+        } else if (response == 'Correct'){
+          $('#warning').text("Login Successful");
+          console.log('uname: ' + username);
+          console.log('pw: ' + password);
+        }
+        $('#username').val('');
+        $('#password').val('');
       }
-      $('#username').val('');
-      $('#password').val('');
+    })
+  } else {
+    $('#warning').text("Please enter a username and password");
+  }
+});
 
-    }
-  })
+
+function removeItem(item) {
+  console.log('remove');
+  if (!username || !password) {
+    $('#warning').text('Please log in first');
+  } else {
+    console.log('hit');
+    var data = "userName=" + username + "&PW=" + password +
+      "&Action=Remove&id=" + item.toString();
+    $.post({
+      url: 'db.php',
+      data: data,
+      success: function(response) {
+        if (response.length == 0) {
+          $('.output').html("No results to show...");
+        } else {
+          $('.output').html(showCurrent(JSON.parse(response)));
+        }
+      }
+    })
+  }
+}
+
+$('.submitNew').click(function() {
+  $('.newCourseInfo').slideUp(400);
+  $('.addNew').slideDown(400);
+})
+
+$('.addNew').click(function() {
+  if (!username || !password) {
+    $('#warning').text('Please log in first');
+  } else {
+  $('.newCourseInfo').slideDown(400);
+  $('.addNew').slideUp(400);
+  }
 })
